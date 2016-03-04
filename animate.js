0dc0e6e1f75648ebe2a3332cd6e94f99e243af8c
Rick Melucci <rimelucci@gmail.com>
2016-03-04 12:32:18 -0500

    paddle is there, almost got it to follow mouse

--- animate.js
+++ animate.js
@@ -1,6 +1,20 @@
 var c = document.getElementById("brick");
+var start = document.getElementById("start");
 var ctx = c.getContext("2d");
-
-ctx.fillStyle= "#000066";
+ctx.fillStyle= "red";
 ctx.strokeStyle = "#000000";
-ctx.strokeRect(0,0,c.width,c.height);
+ctx.fillRect(269, 520, 50, 10);
+var x = 0;
+var y = 0;
+
+var refresh = function(e){
+    x = e.offsetX;
+    y = e.offsetY;
+    ctx.fillRect(x, y, 50, 10);
+}
+
+start.addEventListener("click", function(){
+    console.log("hello");
+    requestId = window.requestAnimationFrame(refresh);
+});
+
